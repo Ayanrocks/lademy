530266681c2785247b07d9a98201c70c5ddeaea8
Ayanrocks <ayanb1999@gmail.com>
2019-02-10 13:33:03 +0530

    testing

--- app.js
+++ app.js
@@ -3,51 +3,59 @@
  *
  */
 
-//Dependencies
+// Dependencies
 
-const express = require("express"),
-  app = express(),
-  mongoose = require("mongoose"),
-  bodyParser = require("body-parser"),
-  path = require("path"),
-  cookieParser = require("cookie-parser"),
-  expressSession = require("express-session"),
-  passport = require("passport"),
-  keys = require("./config/keys.js");
+const express = require('express');
 
-require("./database/connection");
-require("./models/Lectures");
-require("./models/Students");
-require("./models/Teachers");
+const app = express();
 
-require("./services/passport/student");
-require("./services/passport/teachers");
+const bodyParser = require('body-parser');
 
-//Configuration
+const path = require('path');
+
+const cookieParser = require('cookie-parser');
+
+const expressSession = require('express-session');
+const RedisStore = require('connect-redis')(expressSession);
+
+const passport = require('passport');
+
+const keys = require('./config/keys.js');
+
+require('./database/connection');
+require('./models/Lectures');
+require('./models/Students');
+require('./models/Teachers');
+
+require('./services/passport/student');
+require('./services/passport/teachers');
+
+// Configuration
 app.use(cookieParser());
 app.use(
   expressSession({
     secret: keys.sessionSecret,
     resave: false,
-    saveUninitialized: false
-  })
+    saveUninitialized: false,
+    store: new RedisStore(),
+  }),
 );
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static(__dirname + "/client/dist"));
+app.use(express.static(`${__dirname}/client/dist`));
 
-//Routes
-require("./routes/studentAuthRoutes")(app);
+// Routes
+require('./routes/studentAuthRoutes')(app);
 
-if (process.env.NODE_ENV === "production") {
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+if (process.env.NODE_ENV === 'production') {
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
   });
 }
 
-//Listening
+// Listening
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log("Server Started at " + PORT));
+app.listen(PORT, () => console.log(`Server Started at ${PORT}`));
